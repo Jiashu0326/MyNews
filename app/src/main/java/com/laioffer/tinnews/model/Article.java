fe6cecc7397d5fa198bb8783982a600a0db36aa7
Jiashu0326 <jiashu0326@gmail.com>
2021-12-24 02:24:12 -0500

    UPDATE SAVE and DATABASE

--- app/src/main/java/com/laioffer/tinnews/model/Article.java
+++ app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -1,11 +1,19 @@
 package com.laioffer.tinnews.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Article {
     public String author;
     public String content;
     public String description;
     public String publishedAt;
     public String title;
+
+    @NonNull
+    @PrimaryKey
     public String url;
     public String urlToImage;
 }
--- app/src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
@@ -2,13 +2,21 @@ package com.laioffer.tinnews.ui.save;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentSaveBinding;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +27,8 @@ public class SaveFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private FragmentSaveBinding binding;
+    private SaveViewModel viewModel;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -61,6 +71,31 @@ public class SaveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false);
+        binding =  FragmentSaveBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SavedNewsAdapter savedNewsAdapter = new SavedNewsAdapter();
+        binding.newsSavedRecyclerView.setAdapter(savedNewsAdapter);
+        binding.newsSavedRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+
+        NewsRepository repository = new NewsRepository();
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SaveViewModel.class);
+        viewModel
+                .getAllSavedArticles()
+                .observe(
+                        getViewLifecycleOwner(),
+                        savedArticles -> {
+                            if (savedArticles != null) {
+                                Log.d("SaveFragment", savedArticles.toString());
+                                savedNewsAdapter.setArticles(savedArticles);
+                            }
+                        });
+    }
+
 }
